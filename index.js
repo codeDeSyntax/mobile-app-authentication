415a2ebf84b6b18772f67c7e679ecf059b7fd33c
unknown <minwidth49@gmail.com>
2024-04-24 06:34:43 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bcryptjs from 'bcryptjs';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// MongoDB connection
+const mongoUrl = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.4';
+mongoose.connect(mongoUrl)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('MongoDB connection error:', error.message);
+  });
+
+// User schema
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  gender: String,
+});
+const User = mongoose.model('ReactNativeTestUser', userSchema);
+
+// Routes
+app.get('/', (req, res) => {
+  res.send({ message: 'This is a test' });
+});
+
+app.post('/register', async (req, res) => {
+  const { username, email, password, gender } = req.body;
+
+  try {
+    const encryptedPassword = await bcryptjs.hash(password, 10);
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(400).send({ message: 'User already exists' });
+    }
+
+    const newUser = new User({
+      username,
+      email,
+      password: encryptedPassword,
+      gender,
+    });
+
+    await newUser.save();
+    return res.status(201).send({ message: 'User created successfully' });
+  } catch (error) {
+    console.error('Error creating user:', error);
+    return res.status(500).send({ message: 'Internal server error' });
+  }
+});
+
+app.post('/login' , async(req, res) => {
+    const {email , password} = req.body;
+    const registeredUser = await User.findOne({email});
+    const isPasswordValid = bcryptjs.compare(password,registeredUser.password);
+
+    if(registeredUser && isPasswordValid) {
+        return res.status(200).send({message: 'User logged in successfully', status: 'ok'});
+    }
+    })
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
